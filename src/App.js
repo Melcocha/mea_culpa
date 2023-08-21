47f335fb851aa1f78d6522aac87a6317d68d478f
melvin.rodriguez <mr.arteaga06@gmail.com>
2023-08-21 14:44:48 -0600

    commit 8-21-2023

--- src/App.js
+++ src/App.js
@@ -4,17 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>Bienvenido a mi sitio web</h1>
-        <p>Esto es un párrafo de introducción.</p>
-      </header>
-      <main>
-        <h2>Sección Principal</h2>
-        <p>Aquí puedes agregar más contenido, imágenes, enlaces, etc.</p>
-      </main>
-      <footer>
-        <p>Derechos reservados © 2023</p>
-      </footer>
+      
     </div>
   );
 }
---
+++ src/components/NavBar.js
@@ -0,0 +1,46 @@
+import { useEffect, useState } from "react";
+import { Navbar, Container } from "react-bootstrap";
+
+export const NavBar = () => {
+    const [activeLink, setActiveLink]= useState('home');
+    const [scolled, seScrolled] = useState(false);
+    useEffect(() => {
+        const onScroll = () => {
+            if (window.scrollY > 50){
+                seScrolled(true);
+            } else {
+                seScrolled(false);
+            }
+        }
+        window.addEventListener("scroll", onScroll);
+
+        return () => window.removeEventListener("scroll",onScroll);
+    },[])
+    return (
+        <Navbar>
+            <Container>
+                <Navbar.Brand href="#home">
+                  <img src={''} alt="Logo"></img>  
+                </Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav">
+                    <span className="navbar-toggler-icon"></span>
+                </Navbar.Toggle>
+                <Navbar.Collapse id="basic-navbar-nav">
+                <Nav className="me-auto">
+                    <Nav.Link href="#home">Home</Nav.Link>
+                    <Nav.Link href="#link">Skills</Nav.Link>
+                    <Nav.Link href="#link">Projects</Nav.Link>
+                </Nav>
+                <span className="navbar-text">
+                    <div className="social-icon">
+                        <a href="#"><img src={''} alt=" "></img></a>
+                        <a href="#"><img src={''} alt=" "></img></a>
+                        <a href="#"><img src={''} alt=" "></img></a>
+                    </div>
+                    <button className="vvd" onClick={() => console.log('connect')} /><span>Let's Connect</span>
+                </span>
+                </Navbar.Collapse>
+            </Container>    
+        </Navbar>
+    );
+}
