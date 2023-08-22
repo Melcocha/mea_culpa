f012390a2a738355ffdb4a18638263fdf6e7887c
Melcocha <mr.arteaga06@gmail.com>
2023-08-21 22:14:16 -0600

    Commit 23-8-2023

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
+import React from 'react';
+import "./App.css";
+import Header from './components/header/Header';
 
-import './App.css';
-import { NavBar } from './components/NavBar';
-
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      
-    </div>
-  );
+    <>
+      <Header/>
+    </>
+  )
 }
 
-
-export default App;
+export default App
